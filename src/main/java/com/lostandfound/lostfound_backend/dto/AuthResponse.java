3ab10afa85a7ac4fb0d5b30653efc98c5f80e65a
navishd <navishmitha.gunawardana2@gmail.com>
2025-09-17 12:17:49 +0530

    dto

---
+++ src/main/java/com/lostandfound/lostfound_backend/dto/AuthResponse.java
@@ -0,0 +1,10 @@
+package com.lostandfound.lostfound_backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AuthResponse {
+    private String token;
+}
