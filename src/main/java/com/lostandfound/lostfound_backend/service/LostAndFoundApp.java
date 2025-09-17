b03c2e7756c365409a5efb1176b777135cc41889
navishd <navishmitha.gunawardana2@gmail.com>
2025-09-17 12:37:49 +0530

    res

---
+++ src/main/java/com/lostandfound/lostfound_backend/service/LostAndFoundApp.java
@@ -0,0 +1,11 @@
+package com.lostandfound.lostfound_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostAndFoundApp {
+	public static void main(String[] args) {
+		SpringApplication.run(LostAndFoundApp.class, args);
+	}
+}
---
+++ src/test/java/com/lostandfound/lostfound_backend/LostfoundBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lostandfound.lostfound_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostfoundBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
